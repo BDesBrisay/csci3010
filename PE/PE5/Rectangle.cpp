b05282a904404d7182e10f8bafdce801dbe3c6d4
BDesBrisay <bryce@desbrisay.com>
2019-09-27 15:30:49 -0600

    pe5

---
+++ PE/PE5/Rectangle.cpp
@@ -0,0 +1,44 @@
+/*
+Original bugs: Shrink rectangle does not properly modify point1 coordinates
+*/
+
+#include "Rectangle.h"
+
+Rectangle::Rectangle(Point p1, Point p2) {
+  p1_ = p1;
+  p2_ = p2;
+}
+
+int Rectangle::GetWidth() {
+  return p2_.x - p1_.x;
+}
+
+int Rectangle::GetHeight() {
+  return p2_.y - p1_.y;
+} 
+
+bool Rectangle::Overlaps(Rectangle& other) {
+  Point o1 = other.get_p1();
+  Point o2 = other.get_p2();
+
+  // rectangle to the side of the other
+  if (p1_.x > o2.x || p2_.x < o1.x) return false;
+  // rectangle above/below the other
+  if (p1_.y > o2.y || p2_.y < o2.y) return false;
+
+  return true;
+}
+
+int Rectangle::CalculateArea() {
+  return Rectangle::GetWidth() * Rectangle::GetHeight();
+}
+
+void Rectangle::Expand() {
+  p1_ = { x: p1_.x - 1, y: p1_.y - 1 };
+  p2_ = { x: p2_.x + 1, y: p2_.y + 1 };
+}
+
+void Rectangle::Shrink() {
+  p1_ = { x: p1_.x + 1, y: p1_.y + 1 };
+  p2_ = { x: p2_.x - 1, y: p2_.y - 1 };
+}
